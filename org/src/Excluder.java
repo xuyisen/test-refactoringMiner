6cc91b706db929b9cf4d8bd93296f4fb272086e0
xuyisen <xuyisen@whu.edu.cn>
2024-11-25 23:40:35 -0500

    test 3 after

--- org/src/Excluder.java
+++ org/src/Excluder.java
@@ -1,252 +1,77 @@
-/*
- * Copyright (C) 2008 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
-package com.google.gson.internal;
+package com.google.gson;
 
-import com.google.gson.ExclusionStrategy;
-import com.google.gson.FieldAttributes;
-import com.google.gson.Gson;
-import com.google.gson.TypeAdapter;
-import com.google.gson.TypeAdapterFactory;
+import static com.google.common.truth.Truth.assertThat;
+
 import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.Since;
-import com.google.gson.annotations.Until;
-import com.google.gson.reflect.TypeToken;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonWriter;
-import java.io.IOException;
+import com.google.gson.internal.Excluder;
 import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import org.junit.Test;
 
 /**
- * This class selects which fields and types to omit. It is configurable, supporting version
- * attributes {@link Since} and {@link Until}, modifiers, synthetic fields, anonymous and local
- * classes, inner classes, and fields with the {@link Expose} annotation.
- *
- * <p>This class is a type adapter factory; types that are excluded will be adapted to null. It may
- * delegate to another type adapter if only one direction is excluded.
+ * Unit tests for GsonBuilder.REQUIRE_EXPOSE_DESERIALIZE.
  *
  * @author Joel Leitch
- * @author Jesse Wilson
  */
-public final class Excluder implements TypeAdapterFactory, Cloneable {
-    private static final double IGNORE_VERSIONS = -1.0d;
-    public static final Excluder DEFAULT = new Excluder();
+public class ExposeAnnotationExclusionStrategyTest {
+    private Excluder excluder = Excluder.DEFAULT.excludeFieldsWithoutExposeAnnotation();
 
-    private double version = IGNORE_VERSIONS;
-    private int modifiers = Modifier.TRANSIENT | Modifier.STATIC;
-    private boolean serializeInnerClasses = true;
-    private boolean requireExpose;
-    private List<ExclusionStrategy> serializationStrategies = Collections.emptyList();
-    private List<ExclusionStrategy> deserializationStrategies = Collections.emptyList();
-
-    @Override
-    protected Excluder clone() {
-        try {
-            return (Excluder) super.clone();
-        } catch (CloneNotSupportedException e) {
-            throw new AssertionError(e);
-        }
+    @Test
+    public void testNeverSkipClasses() {
+        assertThat(excluder.excludeClass(MockObject.class, true)).isFalse();
+        assertThat(excluder.excludeClass(MockObject.class, false)).isFalse();
     }
 
-    public Excluder withVersion(double ignoreVersionsAfter) {
-        Excluder result = clone();
-        result.version = ignoreVersionsAfter;
-        return result;
+    @Test
+    public void testSkipNonAnnotatedFields() throws Exception {
+        assertFieldExclusion("hiddenField", true, true);
     }
 
-    public Excluder withModifiers(int... modifiers) {
-        Excluder result = clone();
-        result.modifiers = 0;
-        for (int modifier : modifiers) {
-            result.modifiers |= modifier;
-        }
-        return result;
+    @Test
+    public void testSkipExplicitlySkippedFields() throws Exception {
+        assertFieldExclusion("explicitlyHiddenField", true, true);
     }
 
-    public Excluder disableInnerClassSerialization() {
-        Excluder result = clone();
-        result.serializeInnerClasses = false;
-        return result;
+    @Test
+    public void testNeverSkipExposedAnnotatedFields() throws Exception {
+        assertFieldExclusion("exposedField", false, false);
     }
 
-    public Excluder excludeFieldsWithoutExposeAnnotation() {
-        Excluder result = clone();
-        result.requireExpose = true;
-        return result;
+    @Test
+    public void testNeverSkipExplicitlyExposedAnnotatedFields() throws Exception {
+        assertFieldExclusion("explicitlyExposedField", false, false);
     }
 
-    public Excluder withExclusionStrategy(
-            ExclusionStrategy exclusionStrategy, boolean serialization, boolean deserialization) {
-        Excluder result = clone();
-        if (serialization) {
-            result.serializationStrategies = new ArrayList<>(serializationStrategies);
-            result.serializationStrategies.add(exclusionStrategy);
-        }
-        if (deserialization) {
-            result.deserializationStrategies = new ArrayList<>(deserializationStrategies);
-            result.deserializationStrategies.add(exclusionStrategy);
-        }
-        return result;
+    @Test
+    public void testDifferentSerializeAndDeserializeField() throws Exception {
+        Field f = createFieldAttributes("explicitlyDifferentModeField");
+        assertThat(excluder.excludeField(f, true)).isFalse();
+        assertThat(excluder.excludeField(f, false)).isTrue();
     }
 
-    @Override
-    public <T> TypeAdapter<T> create(final Gson gson, final TypeToken<T> type) {
-        Class<?> rawType = type.getRawType();
-        boolean excludeClass = excludeClassChecks(rawType);
-
-        final boolean skipSerialize = excludeClass || excludeClassInStrategy(rawType, true);
-        final boolean skipDeserialize = excludeClass || excludeClassInStrategy(rawType, false);
-
-        if (!skipSerialize && !skipDeserialize) {
-            return null;
-        }
-
-        return new TypeAdapter<T>() {
-            /** The delegate is lazily created because it may not be needed, and creating it may fail. */
-            private TypeAdapter<T> delegate;
-
-            @Override
-            public T read(JsonReader in) throws IOException {
-                if (skipDeserialize) {
-                    in.skipValue();
-                    return null;
-                }
-                return delegate().read(in);
-            }
-
-            @Override
-            public void write(JsonWriter out, T value) throws IOException {
-                if (skipSerialize) {
-                    out.nullValue();
-                    return;
-                }
-                delegate().write(out, value);
-            }
-
-            private TypeAdapter<T> delegate() {
-                TypeAdapter<T> d = delegate;
-                return d != null ? d : (delegate = gson.getDelegateAdapter(Excluder.this, type));
-            }
-        };
+    private void assertFieldExclusion(String fieldName, boolean expectedWhenSerialized, boolean expectedWhenNotSerialized) throws Exception {
+        Field f = createFieldAttributes(fieldName);
+        assertThat(excluder.excludeField(f, true)).isEqualTo(expectedWhenSerialized);
+        assertThat(excluder.excludeField(f, false)).isEqualTo(expectedWhenNotSerialized);
     }
 
-    public boolean excludeField(Field field, boolean serialize) {
-        if ((modifiers & field.getModifiers()) != 0) {
-            return true;
-        }
-
-        if (version != Excluder.IGNORE_VERSIONS
-                && !isValidVersion(field.getAnnotation(Since.class), field.getAnnotation(Until.class))) {
-            return true;
-        }
-
-        if (field.isSynthetic()) {
-            return true;
-        }
-
-        if (requireExpose) {
-            Expose annotation = field.getAnnotation(Expose.class);
-            if (annotation == null || (serialize ? !annotation.serialize() : !annotation.deserialize())) {
-                return true;
-            }
-        }
-
-        if (!serializeInnerClasses && isInnerClass(field.getType())) {
-            return true;
-        }
-
-        if (isAnonymousOrNonStaticLocal(field.getType())) {
-            return true;
-        }
-
-        List<ExclusionStrategy> list = serialize ? serializationStrategies : deserializationStrategies;
-        if (!list.isEmpty()) {
-            FieldAttributes fieldAttributes = new FieldAttributes(field);
-            for (ExclusionStrategy exclusionStrategy : list) {
-                if (exclusionStrategy.shouldSkipField(fieldAttributes)) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
+    private static Field createFieldAttributes(String fieldName) throws Exception {
+        return MockObject.class.getField(fieldName);
     }
 
-    private boolean excludeClassChecks(Class<?> clazz) {
-        if (version != Excluder.IGNORE_VERSIONS
-                && !isValidVersion(clazz.getAnnotation(Since.class), clazz.getAnnotation(Until.class))) {
-            return true;
-        }
+    @SuppressWarnings("unused")
+    private static class MockObject {
+        @Expose public final int exposedField = 0;
 
-        if (!serializeInnerClasses && isInnerClass(clazz)) {
-            return true;
-        }
+        @Expose(serialize = true, deserialize = true)
+        public final int explicitlyExposedField = 0;
 
-        return isAnonymousOrNonStaticLocal(clazz);
-    }
+        @Expose(serialize = false, deserialize = false)
+        public final int explicitlyHiddenField = 0;
 
-    public boolean excludeClass(Class<?> clazz, boolean serialize) {
-        return excludeClassChecks(clazz) || excludeClassInStrategy(clazz, serialize);
-    }
+        @Expose(serialize = true, deserialize = false)
+        public final int explicitlyDifferentModeField = 0;
 
-    private boolean excludeClassInStrategy(Class<?> clazz, boolean serialize) {
-        List<ExclusionStrategy> list = serialize ? serializationStrategies : deserializationStrategies;
-        for (ExclusionStrategy exclusionStrategy : list) {
-            if (exclusionStrategy.shouldSkipClass(clazz)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean isAnonymousOrNonStaticLocal(Class<?> clazz) {
-        return !Enum.class.isAssignableFrom(clazz)
-                && !isStatic(clazz)
-                && (clazz.isAnonymousClass() || clazz.isLocalClass());
-    }
-
-    private static boolean isInnerClass(Class<?> clazz) {
-        return clazz.isMemberClass() && !isStatic(clazz);
-    }
-
-    private static boolean isStatic(Class<?> clazz) {
-        return (clazz.getModifiers() & Modifier.STATIC) != 0;
-    }
-
-    private boolean isValidVersion(Since since, Until until) {
-        return isValidSince(since) && isValidUntil(until);
-    }
-
-    private boolean isValidSince(Since annotation) {
-        if (annotation != null) {
-            double annotationVersion = annotation.value();
-            return version >= annotationVersion;
-        }
-        return true;
-    }
-
-    private boolean isValidUntil(Until annotation) {
-        if (annotation != null) {
-            double annotationVersion = annotation.value();
-            return version < annotationVersion;
-        }
-        return true;
+        public final int hiddenField = 0;
     }
 }
