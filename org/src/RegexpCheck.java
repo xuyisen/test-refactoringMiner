9a9ddb886b278033651333cbc02ff1bf9f471559
xuyisen <xuyisen@whu.edu.cn>
2025-01-28 22:07:51 -0500

    update v2

--- org/src/RegexpCheck.java
+++ org/src/RegexpCheck.java
@@ -20,6 +20,16 @@ public class RegexpCheck {
             msg = ERROR_LIMIT_EXCEEDED_MESSAGE + msg;
         }
 
+        logMessageDetails(lineNumber, msg);
+    }
+
+    /**
+     * Logs the message details based on the line number and message.
+     *
+     * @param lineNumber the line number the message relates to.
+     * @param msg the message to log.
+     */
+    private void logMessageDetails(int lineNumber, String msg) {
         if (illegalPattern) {
             log(lineNumber, MSG_ILLEGAL_REGEXP, msg);
         }
