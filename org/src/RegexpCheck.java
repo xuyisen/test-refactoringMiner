0d5bf2b02341738c5581fc726679fda47fe47723
xuyisen <xuyisen@whu.edu.cn>
2025-01-28 21:44:32 -0500

    init log message

---
+++ org/src/RegexpCheck.java
@@ -0,0 +1,35 @@
+public class RegexpCheck {
+
+
+    /**
+     * Displays the right message.
+     *
+     * @param lineNumber the line number the message relates to.
+     */
+    private void logMessage(int lineNumber) {
+        String msg;
+
+        if (message == null || message.isEmpty()) {
+            msg = format.pattern();
+        }
+        else {
+            msg = message;
+        }
+
+        if (errorCount >= errorLimit) {
+            msg = ERROR_LIMIT_EXCEEDED_MESSAGE + msg;
+        }
+
+        if (illegalPattern) {
+            log(lineNumber, MSG_ILLEGAL_REGEXP, msg);
+        }
+        else {
+            if (lineNumber > 0) {
+                log(lineNumber, MSG_DUPLICATE_REGEXP, msg);
+            }
+            else {
+                log(lineNumber, MSG_REQUIRED_REGEXP, msg);
+            }
+        }
+    }
+}
