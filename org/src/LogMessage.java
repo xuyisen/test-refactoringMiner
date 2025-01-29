03dd5ceb65d30a084dce8f0cb1c821208fd8273d
xuyisen <xuyisen@whu.edu.cn>
2025-01-28 19:11:17 -0500

    log Message case

--- org/src/LogMessage.java
+++ org/src/LogMessage.java
@@ -4,21 +4,6 @@
  * @param lineNumber the line number the message relates to.
  */
 private void logMessage(int lineNumber) {
-    final String msg = getMessage();
-
-    if (illegalPattern) {
-        log(lineNumber, MSG_ILLEGAL_REGEXP, msg);
-    }
-    else {
-        log(lineNumber, MSG_DUPLICATE_REGEXP, msg);
-    }
-}
-/**
- * Provide right message.
- *
- * @return message for violation.
- */
-private String getMessage() {
     String msg;
 
     if (message == null || message.isEmpty()) {
@@ -32,5 +17,15 @@ private String getMessage() {
         msg = ERROR_LIMIT_EXCEEDED_MESSAGE + msg;
     }
 
-    return msg;
+    if (illegalPattern) {
+        log(lineNumber, MSG_ILLEGAL_REGEXP, msg);
+    }
+    else {
+        if (lineNumber > 0) {
+            log(lineNumber, MSG_DUPLICATE_REGEXP, msg);
+        }
+        else {
+            log(lineNumber, MSG_REQUIRED_REGEXP, msg);
+        }
+    }
 }
