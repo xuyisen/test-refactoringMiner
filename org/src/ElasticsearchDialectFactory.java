9682407e8256c8212641440ef0f9d33e061ba5a8
xuyisen <xuyisen@whu.edu.cn>
2025-01-28 22:44:10 -0500

    extract method 2 code before

--- org/src/ElasticsearchDialectFactory.java
+++ org/src/ElasticsearchDialectFactory.java
@@ -1,4 +1,61 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.backend.elasticsearch.dialect.impl;
+
+import java.lang.invoke.MethodHandles;
+import java.util.OptionalInt;
+
+import org.hibernate.search.backend.elasticsearch.ElasticsearchVersion;
+import org.hibernate.search.backend.elasticsearch.dialect.model.impl.Elasticsearch56ModelDialect;
+import org.hibernate.search.backend.elasticsearch.dialect.model.impl.Elasticsearch6ModelDialect;
+import org.hibernate.search.backend.elasticsearch.dialect.model.impl.Elasticsearch7ModelDialect;
+import org.hibernate.search.backend.elasticsearch.dialect.model.impl.ElasticsearchModelDialect;
+import org.hibernate.search.backend.elasticsearch.dialect.protocol.impl.Elasticsearch56ProtocolDialect;
+import org.hibernate.search.backend.elasticsearch.dialect.protocol.impl.Elasticsearch60ProtocolDialect;
+import org.hibernate.search.backend.elasticsearch.dialect.protocol.impl.Elasticsearch63ProtocolDialect;
+import org.hibernate.search.backend.elasticsearch.dialect.protocol.impl.Elasticsearch64ProtocolDialect;
+import org.hibernate.search.backend.elasticsearch.dialect.protocol.impl.Elasticsearch67ProtocolDialect;
+import org.hibernate.search.backend.elasticsearch.dialect.protocol.impl.Elasticsearch70ProtocolDialect;
+import org.hibernate.search.backend.elasticsearch.dialect.protocol.impl.ElasticsearchProtocolDialect;
+import org.hibernate.search.backend.elasticsearch.logging.impl.Log;
+import org.hibernate.search.util.common.AssertionFailure;
+import org.hibernate.search.util.common.logging.impl.LoggerFactory;
+
+/**
+ * Allows to create an Elasticsearch dialect by detecting the version of a remote cluster.
+ */
 public class ElasticsearchDialectFactory {
+
+    private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
+
+    public ElasticsearchModelDialect createModelDialect(ElasticsearchVersion version) {
+        int major = version.major();
+        OptionalInt minorOptional = version.minor();
+
+        if ( major < 5 ) {
+            throw log.unsupportedElasticsearchVersion( version );
+        }
+        else if ( major == 5 ) {
+            if ( !minorOptional.isPresent() ) {
+                throw log.ambiguousElasticsearchVersion( version );
+            }
+            int minor = minorOptional.getAsInt();
+            if ( minor < 6 ) {
+                throw log.unsupportedElasticsearchVersion( version );
+            }
+            return new Elasticsearch56ModelDialect();
+        }
+        else if ( major == 6 ) {
+            return new Elasticsearch6ModelDialect();
+        }
+        else {
+            return new Elasticsearch7ModelDialect();
+        }
+    }
 
     public ElasticsearchProtocolDialect createProtocolDialect(ElasticsearchVersion version) {
         int major = version.major();
@@ -48,4 +105,5 @@ public class ElasticsearchDialectFactory {
             return new Elasticsearch70ProtocolDialect();
         }
     }
+
 }
